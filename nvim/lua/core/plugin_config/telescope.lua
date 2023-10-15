347d7163970a4907fc406432b8881976d6a2e21a
Chris Tobolski <chris@canopy.security>
2023-10-15 01:28:17 -0400

    Update telescope config

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -1,16 +1,88 @@
-require('telescope').setup {
-  pickers = {
-    find_files = {
-      hidden = true
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local cmd = vim.cmd
+
+telescope.setup({
+  defaults = {
+    hl_result_eol = false,
+    layout_config = {
+      height = 0.8,
+      prompt_position = "top",
+      preview_width = 0.5,
+      width = 0.9,
     },
-    live_grep = {
-      hidden = true
-    }
-  }
-}
+    mappings = {
+      i = {
+        ["<ESC>"] = actions.close,
+        ["<PageUp>"] = actions.preview_scrolling_up,
+        ["<PageDown>"] = actions.preview_scrolling_down,
+        ["<C-q>"] = false,
+        ["<A-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        ["<A-d>"] = require("telescope.actions").delete_buffer,
+        ["<C-f>"] = actions.results_scrolling_down,
+        ["<C-b>"] = actions.results_scrolling_up,
+      },
+    },
+    multi_icon = "✚",
+    prompt_prefix = "❯ ",
+    selection_caret = "▶ ",
+    sorting_strategy = "ascending",
+  },
+  pickers = {
+    buffers = {
+      show_all_buffers = true,
+    },
+  },
+})
 
-local builtin = require('telescope.builtin')
+telescope.load_extension("fzf")
+-- telescope.load_extension("possession")
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
-vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
+-- Mappings.
+local map = vim.keymap.set
+map("n", "<Space><Space>", require("telescope.builtin").find_files)
+map("n", "<Space>.", function()
+  require("telescope.builtin").find_files({ cwd = "%:h" })
+end)
+map("n", "<Space>,", require("telescope.builtin").buffers)
+map("n", "<Space>'", require("telescope.builtin").current_buffer_fuzzy_find)
+map("n", "<Space>r", require("telescope.builtin").resume)
+map("n", "<Space>c", require("util.telescope-commands").git_bcommits)
+map("n", "<Space>g", require("util.telescope-commands").git_status)
+map("n", "<Space>h", require("telescope.builtin").help_tags)
+map("n", "<Space>i", require("telescope.builtin").highlights)
+map("n", "<Space>o", require("telescope.builtin").oldfiles)
+map("n", "<Space>]", require("telescope.builtin").tags)
+map("n", "<Space>/", function()
+  require("telescope.builtin").grep_string({ search = vim.fn.input("grep ❯ ") })
+end)
+
+-- Demo on how to set cmds per project
+-- local opts = { silent = true }
+-- if vim.fn.filereadable("config/routes.rb") ~= 0 then
+--   map("n", "<Space>ec", ":Telescope find_files cwd=app/controllers<CR>", opts)
+--   map("n", "<Space>eh", ":Telescope find_files cwd=app/helpers<CR>", opts)
+--   map("n", "<Space>ei", ":Telescope find_files cwd=config/initializers<CR>", opts)
+--   map("n", "<Space>em", ":Telescope find_files cwd=app/models<CR>", opts)
+--   map("n", "<Space>es", ":Telescope find_files cwd=app/assets/styles<CR>", opts)
+--   map("n", "<Space>et", ":Telescope find_files cwd=spec<CR>", opts)
+--   map("n", "<Space>ev", ":Telescope find_files cwd=app/views<CR>", opts)
+-- elseif vim.fn.filereadable("src/index.js") ~= 0 then
+--   map("n", "<Space>ec", ":Telescope find_files cwd=src/components<CR>", opts)
+--   map("n", "<Space>es", ":Telescope find_files cwd=src/styles<CR>", opts)
+--   map("n", "<Space>et", ":Telescope find_files cwd=src/__tests__/components<CR>", opts)
+-- end
+
+-- Extension mappings.
+-- map("n", "<Space>p", require('telescope').extensions.possession.list)
+
+-- Workaround Telescope highlights bug: https://is.gd/Sjxegs
+autocmd("User", {
+  pattern = "TelescopePreviewerLoaded",
+  callback = function()
+    vim.opt_local.splitkeep = "cursor"
+  end,
+  group = augroup("TelescopePluginEvents", {})
+})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -98,6 +98,17 @@ local plugins = {
     },
     event = "VeryLazy",
   },
+  -----------------------------
+  -- Fuzzy
+  -----------------------------
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    keys = { "<Space>" },
+  },
   -- {
   --   "nvim-tree/nvim-tree.lua",
   --   version = "*",
