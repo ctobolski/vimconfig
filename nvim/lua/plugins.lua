f49233997b7c861cdd8e8bc9e56a045bb597abd3
Chris Tobolski <chris@canopy.security>
2023-10-15 01:16:51 -0400

    shamelessly steal from bluz

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -11,89 +11,162 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  -- Appearance
-  'nvim-lualine/lualine.nvim',
-  --'bluz71/vim-nightfly-colors',
-  'ellisonleao/gruvbox.nvim',
-  {
-    'dracula/vim',
-    lazy = false,
+local lazy = require("lazy")
+local view_config = require("lazy.view.config")
+
+local configuration = {
+  install = {
+    colorscheme = { "gruvbox" }
   },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip", "netrwPlugin", "rplugin", "tarPlugin", "tohtml", "tutor",
+        "zipPlugin",
+      },
+    }
+  },
+  ui = {
+    border = "single",
+    icons = {
+      cmd = "❯",
+      config = "✹",
+      event = "♢",
+      ft = "*",
+      init = "⏻",
+      keys = "♫",
+      plugin = "□",
+      runtime = "○",
+      source = "‹›",
+      start = "▷",
+      task = "✔ ",
+      lazy = "⠤⠤ ",
+      list = { "●", "→", "◉", "‒" },
     },
-    config = function()
-      require("nvim-tree").setup {}
-    end,
   },
-  'https://github.com/nvim-lua/plenary.nvim',
-  'christoomey/vim-tmux-navigator', -- enables switching between vim and tmux panes using the same keybindings <3
-  --Toolbox
+}
+
+view_config.keys.close = "<Esc>"
+view_config.keys.profile_filter = "<C-p>"
+
+local plugins = {
+  -----------------------------
+  -- Colorscheme
+  -----------------------------
+  'ellisonleao/gruvbox.nvim',
+  {   'bluz71/vim-nightfly-colors', lazy = true },
+  { 'dracula/vim', lazy = true },
+  -----------------------------
+  -- Appearance
+  -----------------------------
+  { 'bluz71/nvim-linefly' },
+  {
+    "nvim-tree/nvim-web-devicons",
+    event = "VeryLazy",
+  },
+    {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "BufReadPre",
+  },
+  -----------------------------
+  -- Behavior
+  -----------------------------
+  "rhysd/clever-f.vim", -- ~/dotfiles/nvim/after/plugin/clever-f.lua
+  "wellle/targets.vim",
+  {
+    "tpope/vim-unimpaired",
+    keys = { "[", "]" },
+  },
   'tpope/vim-fugitive',
   'tpope/vim-surround',
   'tpope/vim-repeat',
   'tpope/vim-commentary',
-  "github/copilot.vim", -- a little help from our friend
-  --Language Support
-  'nvim-treesitter/nvim-treesitter', -- parsers for syntax highlighting
-  "williamboman/mason.nvim", -- language server manager
-  "williamboman/mason-lspconfig.nvim", -- glue between mason and nvim-lspconfig
-  "neovim/nvim-lspconfig", -- simply sets up config for a language server, server must be installed by something like mason.nvim
-  'hrsh7th/cmp-nvim-lsp', --provide improved completion for neovims lsp
+  -----------------------------
+  -- Filesystem
+  -----------------------------
   {
-    "nvimdev/lspsaga.nvim",
-    config = function() 
-      require('lspsaga').setup({
-        code_action_icon = "💡",
-        symbol_in_winbar = {
-          in_custom = false,
-          enable = true,
-          separator = ' ',
-          show_file = true,
-          file_formatter = ""
-        }
-      })
-    end
-  },
-  {
-    "ray-x/lsp_signature.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        "s1n7ax/nvim-window-picker",
+        version = "2.*",
+      },
+    },
     event = "VeryLazy",
   },
-  "jose-elias-alvarez/null-ls.nvim",
-  'lewis6991/gitsigns.nvim',
-  'hrsh7th/nvim-cmp',
-  'L3MON4D3/LuaSnip',
-  'saadparwaiz1/cmp_luasnip',
-  "rafamadriz/friendly-snippets",
-  "MunifTanjim/prettier.nvim",
-  {
-    "toppair/peek.nvim", -- preview code in popup window like markdown files
-    build = "deno task --quiet build:fast",
-  },
-  {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      -- calling `setup` is optional for customization
-      require("fzf-lua").setup({})
-    end
-  },
   -- {
-  -- 'nvim-telescope/telescope.nvim',
-  -- tag = '0.1.0',
-  -- dependencies = { {'nvim-lua/plenary.nvim'} }
+  --   "nvim-tree/nvim-tree.lua",
+  --   version = "*",
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   config = function()
+  --     require("nvim-tree").setup {}
+  --   end,
   -- },
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = { {'kevinhwang91/promise-async'} }
-  }
+  --'https://github.com/nvim-lua/plenary.nvim',
+  --'christoomey/vim-tmux-navigator', -- enables switching between vim and tmux panes using the same keybindings <3
+  ----Toolbox
+  --"github/copilot.vim", -- a little help from our friend
+  ----Language Support
+  --'nvim-treesitter/nvim-treesitter', -- parsers for syntax highlighting
+  --"williamboman/mason.nvim", -- language server manager
+  --"williamboman/mason-lspconfig.nvim", -- glue between mason and nvim-lspconfig
+  --"neovim/nvim-lspconfig", -- simply sets up config for a language server, server must be installed by something like mason.nvim
+  --'hrsh7th/cmp-nvim-lsp', --provide improved completion for neovims lsp
+  --{
+  --  "nvimdev/lspsaga.nvim",
+  --  config = function() 
+  --    require('lspsaga').setup({
+  --      code_action_icon = "💡",
+  --      symbol_in_winbar = {
+  --        in_custom = false,
+  --        enable = true,
+  --        separator = ' ',
+  --        show_file = true,
+  --        file_formatter = ""
+  --      }
+  --    })
+  --  end
+  --},
+  --{
+  --  "ray-x/lsp_signature.nvim",
+  --  event = "VeryLazy",
+  --},
+  --"jose-elias-alvarez/null-ls.nvim",
+  --'lewis6991/gitsigns.nvim',
+  --'hrsh7th/nvim-cmp',
+  --'L3MON4D3/LuaSnip',
+  --'saadparwaiz1/cmp_luasnip',
+  --"rafamadriz/friendly-snippets",
+  --"MunifTanjim/prettier.nvim",
+  --{
+  --  "toppair/peek.nvim", -- preview code in popup window like markdown files
+  --  build = "deno task --quiet build:fast",
+  --},
+  --{
+  --  "ibhagwan/fzf-lua",
+  --  -- optional for icon support
+  --  dependencies = { "nvim-tree/nvim-web-devicons" },
+  --  config = function()
+  --    -- calling `setup` is optional for customization
+  --    require("fzf-lua").setup({})
+  --  end
+  --},
+  ---- {
+  ---- 'nvim-telescope/telescope.nvim',
+  ---- tag = '0.1.0',
+  ---- dependencies = { {'nvim-lua/plenary.nvim'} }
+  ---- },
+  --{
+  --  'kevinhwang91/nvim-ufo',
+  --  dependencies = { {'kevinhwang91/promise-async'} }
+  --}
 }
 
-local opts = {}
 
-require("lazy").setup(plugins, opts)
+lazy.setup(plugins, configuration)
